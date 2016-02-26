8753c433bc985bf356cfa2a3b6b6e583c234d9ec
akvince <deflandre.vincent@live.fr>
2016-02-26 16:12:34 +0100

    ajout du signIn $

--- chat-akv2.js
+++ chat-akv2.js
@@ -3,14 +3,29 @@ Messages = new Mongo.Collection('messages');
 if (Meteor.isClient) {
 
   Template.messages.helpers({
-    messages: function () {
-      return Messages.find({message: "Hello world"});
+    messages: function() {
+      return Messages.find({});
     }
   });
+
+  Template.messages.events({
+    'keypress textarea': function(e, instance) {
+      if (e.keyCode == 13) { // enter key pressed
+        var value = instance.find('textarea').value;
+        instance.find('textarea').value = '';
+
+        Messages.insert({
+          message: value,
+          timestamp: new Date()
+            // user: Meteor.userId()
+        });
+      }
+    }
+  })
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
+  Meteor.startup(function() {
     // code to run on server at startup
   });
 }
